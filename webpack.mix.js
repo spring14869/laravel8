fd6b42f590935f58fd4cf87965919e4546ff32d6
Marley <marchang@elta.com.tw>
2022-03-10 12:08:50 +0800

    <feat>: npm run dev | laravel.mix webpack 設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,13 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.setPublicPath('public/assets'); //套件基本路徑，避免compiling路徑四散
+mix.setResourceRoot('../');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/assets/js')
+    .sourceMaps(false, 'source-map')
+    .postCss('resources/css/app.css', 'public/assets/css', [
         //
-    ]);
+    ])
+    .postCss('resources/css/dashboard.css', 'public/assets/css')
+    .copy('node_modules/font-awesome/fonts/', 'public/assets/fonts');
